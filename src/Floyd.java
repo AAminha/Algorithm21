65ffb0b993a5599ebf6ae446556e2e8df56664bb
AAminha <87255791+AAminha@users.noreply.github.com>
2021-11-03 00:16:45 +0900

    첫번째 알고리즘 과제2

--- src/Floyd.java
+++ src/Floyd.java
@@ -1,11 +1,23 @@
 public class Floyd {
     final static int INF = 99999;
-    static int[][] W = {
+
+    // 교재 입력 데이터 (Figure 3.2)
+    static int[][] WW = {
         {0, 1, INF, 1, 5},
         {9, 0, 3, 2, INF},
         {INF, INF, 0, 4, INF},
         {INF, INF, 2, 0, 3},
         {3, INF, INF, INF, 0}
+    };
+
+    // 임의의 자작 데이터
+    static int[][] W = {
+        {0,7,4,INF,INF,1},
+        {2,0,5,INF,5,INF},
+        {INF,INF,0,5,INF,INF},
+        {INF,2,INF,0,5,4},
+        {INF,3,INF,INF,0,2},
+        {4,INF,1,INF,5,0}
     };
 
     // 배열 원소 출력 함수
@@ -30,12 +42,7 @@ public class Floyd {
         int[][] D = new int[n][n];  // 최단거리행렬 선언
         int[][] P = new int[n][n];
 
-        for (i = 0; i < n; i++) {
-            for (j = 0; j < n; j++) {
-                P[i][j] = 0;
-            }
-        }
-
+        // 배열 W을 배열 D에 복사하는 과정
         for (i = 0; i < n; i++) {
             for (j = 0; j < n; j++) {
                 D[i][j] = W[i][j];
@@ -53,6 +60,7 @@ public class Floyd {
             }
         }
 
+        // 배열 출력
         System.out.println("<< 배열 W >>");
         printItem(W);
         System.out.println("<< 배열 D >>");
--- src/Path.java
+++ src/Path.java
@@ -1,10 +1,18 @@
 public class Path {
-    static int[][] P = new int[][] {
+    static int[][] PP = new int[][] {
         {0, 0, 4, 0, 4},
         {5, 0, 0, 0, 4},
         {5, 5, 0, 0, 4},
         {5, 5, 0, 0, 0},
         {0, 1, 4, 1, 0}
+    };
+    static int[][] P = new int[][] {
+        {0,0,6,6,6,0},
+        {0,0,6,6,0,1},
+        {4,4,0,0,4,4},
+        {2,0,6,0,0,0},
+        {2,0,6,6,0,0},
+        {0,4,0,3,0,0}
     };
 
     public static void path(int q, int r) {
@@ -15,8 +23,8 @@ public class Path {
         }
     }
     public static void main(String[] args) {
-        int s = 5;  // 시작 정점
-        int e = 3;  // 도착 정점
+        int s = 1;  // 시작 정점
+        int e = 4;  // 도착 정점
         path(s-1, e-1);
     }
 }
